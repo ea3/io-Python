eab477e3e89c5192a0d045cedd5e53af3a0ae1f6
Emilio Araos <ea3dev@gmail.com>
2020-01-10 14:23:44 -0700

    readlines(), seek(), reading text file

---
+++ io.py
@@ -0,0 +1,10 @@
+myfile = open('test.txt')
+print(myfile.read())
+print(myfile.read())
+myfile.read()
+print(myfile.read())
+myfile.seek(0)
+print(myfile.read())
+myfile.seek(0)
+print(myfile.readlines())
+
