63998cbeb89f0ba5d41b176c4cba132c9cf35830
Emilio Araos <ea3dev@gmail.com>
2020-01-10 14:40:51 -0700

    with open() as..

--- io.py
+++ io.py
@@ -7,4 +7,23 @@ myfile.seek(0)
 print(myfile.read())
 myfile.seek(0)
 print(myfile.readlines())
+myfile.close()
+with open('test.txt') as my_new_file:
+	contents = my_new_file.read()
+print(contents)
+
+with open('test.txt', mode= 'r') as myfile:
+	contents = myfile.read()
+print(contents)	
+
+with open('my_new_file.txt',mode='r') as f:
+	print(f.read())       				# Read mode
+
+
+with open('my_new_file.txt', mode = 'a') as f:
+	f.write('FOUR ON FOURTH')
+
+with open('my_new_file.txt',mode='r') as f:
+	print(f.read())  	
+
 
